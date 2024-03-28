dfc358e1e0c2fe5247850c3790be7b1763aa3009
artemz0402 <156719952+artemz0402@users.noreply.github.com>
2024-03-27 20:16:00 -0400

    Add files via upload

---
+++ Module9Schemata.sql
@@ -0,0 +1,52 @@
+ CREATE TABLE employees (
+    "emp_no" int   NOT NULL,
+	"emp_title_id" varchar   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" varchar   NOT NULL,
+    "last_name" varchar   NOT NULL,
+    "sex" char(1)   NOT NULL,
+    "hire_date" date   NOT NULL,
+   	primary key (emp_no)
+);
+
+CREATE TABLE "departments" (
+    "dept_no" varchar   NOT NULL,
+    "dept_name" varchar   NOT NULL,
+    PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "titles" (
+    "title_id" text NOT NULL,
+    "title" varchar NOT NULL, 
+    PRIMARY KEY (
+        "title_id"
+    )
+);
+
+
+CREATE TABLE "dept_emp" (
+    "emp_no" int   NOT NULL,
+    "dept_no" varchar   NOT NULL,
+    PRIMARY KEY (
+        "emp_no","dept_no"
+     )
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" int   NOT NULL,
+    "salary" int   NOT NULL,
+    PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" varchar   NOT NULL,
+    "emp_no" int   NOT NULL,
+    PRIMARY KEY (
+        "dept_no"
+     )
+);
+
